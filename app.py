601bd99a4fff46e5e4c28980f9835dfc132c8e38
Matthieu Fatrez <matthieu.fatrez@suse.com>
2023-06-05 13:55:11 +0200

    remove mariadb to test without

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template
-import mariadb
+# import mariadb
 
 app = Flask(__name__)
 
-conn = mariadb.connect(host='172.16.3.156',
-                       port= 3306,
-                       user='docapostedemo',
-                       password='d0cAp0st3_dem0!',
-                       database='db_docaposte')
-cur = conn.cursor()
+# conn = mariadb.connect(host='172.16.3.156',
+#                        port= 3306,
+#                        user='docapostedemo',
+#                        password='d0cAp0st3_dem0!',
+#                        database='db_docaposte')
+# cur = conn.cursor()
 
 @app.route('/')
 def index():

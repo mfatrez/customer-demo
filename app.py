e68742a49b690a6966e84210deadd67cead0091f
Matthieu Fatrez <matthieu.fatrez@suse.com>
2023-06-07 01:05:55 +0200

    test mariadb

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
+import mariadb
+import sys
+
 from flask import Flask, render_template
-# import mariadb
 
 app = Flask(__name__)
 
-conn = mariadb.connect(host='10.77.4.145',
-                       port= 3306,
-                       user='docapostedemo',
-                       password='d0cAp0st3_dem0!',
-                       database='db_docaposte')
+try:
+    conn = mariadb.connect(
+        user="docapostedemo",
+        password="d0cAp0st3_dem0!",
+        host="10.77.4.145",
+        port=3306,
+        database="db_docaposte"
+
+    )
+except mariadb.Error as e:
+    print(f"Error connecting to MariaDB Platform: {e}")
+    sys.exit(1)
+
 cur = conn.cursor()
 
 @app.route('/')

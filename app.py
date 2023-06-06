57e122accde17ce5a4d8ef202c1b7ba4ace8fbd5
Matthieu Fatrez <matthieu.fatrez@suse.com>
2023-06-07 01:01:35 +0200

    test mariadb

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# conn = mariadb.connect(host='172.16.3.156',
-#                        port= 3306,
-#                        user='docapostedemo',
-#                        password='d0cAp0st3_dem0!',
-#                        database='db_docaposte')
-# cur = conn.cursor()
+conn = mariadb.connect(host='10.77.4.145',
+                       port= 3306,
+                       user='docapostedemo',
+                       password='d0cAp0st3_dem0!',
+                       database='db_docaposte')
+cur = conn.cursor()
 
 @app.route('/')
 def index():

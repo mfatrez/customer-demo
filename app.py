5b5769f0516ebff079af7930f8b5ebf3f9a1faed
Matthieu Fatrez <matthieu.fatrez@suse.com>
2023-06-07 02:30:30 +0200

    last test before sleeping

--- app.py
+++ app.py
@@ -5,25 +5,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-conn = "";
-
-try:
-    conn = mariadb.connect(
-        user="docapostedemo",
-        password="d0cAp0st3_dem0!",
-        host="10.77.4.145",
-        port=3306,
-        database="db_docaposte"
-    )
-except mariadb.Error as e:
-    print(f"Error connecting to MariaDB Platform: {e}")
-    sys.exit(1)
-
-cur = conn.cursor()
+config = {
+        'host': '10.77.4.145',
+        'port': 3306,
+        'user': 'docapostedemo',
+        'password': 'd0cAp0st3_dem0!',
+        'database': 'db_docaposte'
+}
 
 @app.route('/')
 def index():
-    print("INSERT INTO doca_test (id, data) VALUES (null, 1)")
+    conn = mariadb.connect(**config)
+    cur = conn.cursor()
     cur.execute("INSERT INTO doca_test (id, data) VALUES (null, 1)")
     conn.commit()
 
